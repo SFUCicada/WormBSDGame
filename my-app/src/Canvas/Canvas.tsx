b09e59d025c9ed0467420f93e0b6702ed4d1203d
Shannon Schmitt <83383502+SFUCicada@users.noreply.github.com>
2022-04-14 14:22:24 -0400

    canvas created

---
+++ my-app/src/Canvas/Canvas.tsx
@@ -0,0 +1,39 @@
+import React, {forwardRef, useEffect} from "react";
+import * as S from './Canvas.styles';
+
+type CanvasProps  = React.DetailedHTMLProps<React.CanvasHTMLAttributes<HTMLCanvasElement>, HTMLCanvasElement> &{
+    draw: (context: CanvasRenderingContext2D) => void;
+};
+
+
+const Canvas = forwardRef<HTMLCanvasElement, CanvasProps>(
+    ({draw, ...props}, canvasRef) => {
+
+       useEffect(() => {
+
+        if(!canvasRef){
+            return;
+        }
+        const canvas = (canvasRef as React.RefObject<HTMLCanvasElement>).current;
+        if(!canvas){
+            return;
+        }
+
+        const context = canvas.getContext('2d');
+        if(!context){
+            return;
+        }
+
+        draw(context);
+
+       }, [draw, canvasRef]);
+       
+        if(!canvasRef){
+            return null;
+        }
+
+        return(<S.Canvas ref={canvasRef as any} {...props}/>);
+    }
+);
+
+export default Canvas
---
+++ my-app/src/Game/Game.tsx
@@ -0,0 +1,20 @@
+import React, { useRef } from "react";
+import Canvas from '../Canvas/Canvas';
+import { GameWrapper } from "./Game.styles";
+
+interface GameProps {}
+
+const Game: React.FC<GameProps> = ({}) => {
+
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+
+    const draw = (ctx: CanvasRenderingContext2D) => {};
+
+    return (
+    <GameWrapper>
+        <Canvas ref={canvasRef}draw={draw} />
+    </GameWrapper>
+    );
+};
+
+export default Game;
